6e19c49db14b084a5d2d7327f3c1c6ff1550bf92
Monochrome Taisa <44545985+idm1303@users.noreply.github.com>
2023-05-14 22:07:52 +0800

    some update

--- src/views/Master/IdentitasUpdate.vue
+++ src/views/Master/IdentitasUpdate.vue
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 import { defineComponent } from 'vue';
 import VueDatePicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css';
@@ -10,75 +10,103 @@ export default defineComponent({
   components: { VueDatePicker },
   data() {
     return {
-      date: null,
-      kodeInstitusi: "",
-      kodeHukum: "",
-      namaInstitusi: "",
-      alamat: "",
-      kota: "",
-      kodePos: "",
-      telepon: "",
-      email: "",
-      website: "",
-      tanggalBerdiri: "",
-      noAkta: "",
-      noSah: "",
+      formData: {
+        Identitas_ID: "",
+        KodeHukum: "",
+        Nama_Identitas: "",
+        Alamat1: "",
+        Kota: "",
+        KodePos: "",
+        Telepon: "",
+        Email: "",
+        Website: "",
+        TglMulai: "",
+        NoAkta: "",
+        NoSah: "",
+      },
+      submitted: false,
     };
   },
   created() {
     this.getIdentitas();
   },
   methods: {
-    uploadFile(event) {
-      const file = event.target.files[0];
-      // call your upload function here, passing the file as an argument
+    // debugging
+    handleSubmit(event: { preventDefault: () => void; }) {
+      event.preventDefault();
+      // Do something with the submitted data (e.g., send it to the server)
+      console.log(this.formData);
+      const id = this.$route.params.id;
+      console.log(this.$route.params.id);
+
+      let TglMulai = this.formData.TglMulai.substring(0, 10);
+      console.log(TglMulai);
+
+      // Show the submitted data
+      this.submitted = true;
     },
+    // uploadFile(event: { target: { files: any[]; }; }) {
+    //   const file = event.target.files[0];
+    //   // call your upload function here, passing the file as an argument
+    // },
 
     // get identitas
     async getIdentitas() {
       try {
         const response = await axios.get("http://localhost:5000/identitas");
-        this.kodeInstitusi = response.data[0].Identitas_ID;
-        this.kodeHukum = response.data[0].KodeHukum;
-        this.namaInstitusi = response.data[0].Nama_Identitas;
-        this.alamat = response.data[0].Alamat1;
-        this.kota = response.data[0].Kota;
-        this.kodePos = response.data[0].KodePos;
-        this.telepon = response.data[0].Telepon;
-        this.email = response.data[0].Email;
-        this.website = response.data[0].Website;
-        this.tanggalBerdiri = response.data[0].TglMulai;
-        this.noAkta = response.data[0].NoAkta;
-        this.noSah = response.data[0].NoSah;
+
+        this.formData.Identitas_ID = response.data[0].Identitas_ID;
+        this.formData.KodeHukum = response.data[0].KodeHukum;
+        this.formData.Nama_Identitas = response.data[0].Nama_Identitas;
+        this.formData.Alamat1 = response.data[0].Alamat1;
+        this.formData.Kota = response.data[0].Kota;
+        this.formData.KodePos = response.data[0].KodePos;
+        this.formData.Telepon = response.data[0].Telepon;
+        this.formData.Email = response.data[0].Email;
+        this.formData.Website = response.data[0].Website;
+        this.formData.TglMulai = response.data[0].TglMulai;
+        this.formData.NoAkta = response.data[0].NoAkta;
+        this.formData.NoSah = response.data[0].NoSah;
       } catch (error) {
         console.log(error);
       }
     },
+
     // update identitas
     async updateIdentitas() {
       try {
         const id = this.$route.params.id;
+        let TglMulai = this.formData.TglMulai.substring(0, 10);
+        
         await axios.put(
           `http://localhost:5000/identitas/${id}`, 
           {
-            Identitas_ID:this.kodeInstitusi,
-            KodeHukum:this.kodeHukum,
-            Nama_Identitas:this.namaInstitusi,
-            Alamat1:this.alamat,
-            Kota:this.kota,
-            KodePos:this.kodePos,
-            Telepon:this.telepon,
-            Email:this.email,
-            Website:this.website,
-            TglMulai:this.tanggalBerdiri,
-            NoAkta:this.noAkta,
-            NoSah:this.noSah,
+            Identitas_ID:this.formData.Identitas_ID,
+            KodeHukum:this.formData.KodeHukum,
+            Nama_Identitas:this.formData.Nama_Identitas,
+            Alamat1:this.formData.Alamat1,
+            Kota:this.formData.Kota,
+            KodePos:this.formData.KodePos,
+            Telepon:this.formData.Telepon,
+            Email:this.formData.Email,
+            Website:this.formData.Website,
+            TglMulai:TglMulai,
+            NoAkta:this.formData.NoAkta,
+            NoSah:this.formData.NoSah,
+          },
+          {
+            timeout: 300000, // Increase timeout to 10 seconds (adjust as needed)
           }
         );
-        ( this.kodeInstitusi = "" ), ( this.kodeHukum = "" ), ( this.namaInstitusi = "" ), ( this.alamat = "" ), ( this.kota = "" ), ( this.kodePos = "" ), ( this.telepon = "" ), ( this.email = "" ), ( this.website = "" ), ( this.tanggalBerdiri = "" ), ( this.noAkta = "" ), ( this.noSah = "" );
+        ( this.formData.Identitas_ID = "" ), ( this.formData.KodeHukum = "" ), ( this.formData.Nama_Identitas = "" ), ( this.formData.Alamat1 = "" ), ( this.formData.Kota = "" ), ( this.formData.KodePos = "" ), ( this.formData.Telepon = "" ), ( this.formData.Email = "" ), ( this.formData.Website = "" ), ( TglMulai = ""), ( this.formData.NoAkta = "" ), ( this.formData.NoSah = "" );
         this.$router.push("/master/identitas-institusi");
-      } catch (error) {
-        console.log(error);
+      } catch (error: any) {
+        console.log(error)
+        if (error.code === 'ECONNABORTED') {
+          console.log('Request timeout:', error.message);
+        } else {
+          console.error('Error:', error.message);
+        }
       }
     },
   },
@@ -87,7 +115,7 @@ export default defineComponent({
 
 <template>
   <main class="bg-gray-100 p-4">
-    <form class="mt-4">
+    <form class="mt-4" @submit="updateIdentitas">
       <div class="shadow rounded overflow-hidden">
         <div class="bg-white px-4 py-5 sm:p-6">
           <div class="grid grid-cols-6 gap-6">
@@ -104,7 +132,7 @@ export default defineComponent({
                 id="institute-code"
                 autoComplete="institute-code"
                 class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                v-model="kodeInstitusi"
+                v-model="formData.Identitas_ID"
               />
             </div>
 
@@ -121,7 +149,7 @@ export default defineComponent({
                 id="law-code"
                 autoComplete="law-code"
                 class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                v-model="kodeHukum"
+                v-model="formData.KodeHukum"
               />
             </div>
 
@@ -138,7 +166,7 @@ export default defineComponent({
                 id="nama-institusi"
                 autoComplete="institusi"
                 class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                v-model="namaInstitusi"
+                v-model="formData.Nama_Identitas"
               />
             </div>
 
@@ -155,24 +183,24 @@ export default defineComponent({
                 id="alamat"
                 autoComplete="alamat"
                 class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                v-model="alamat"
+                v-model="formData.Alamat1"
               />
             </div>
             
             <div class="col-span-6 sm:col-span-4 lg:col-span-3">
               <label
-                htmlFor="kota"
+                htmlFor="Kota"
                 class="block text-sm font-medium text-gray-700"
               >
                 Kota
               </label>
               <input
                 type="text"
-                name="kota"
-                id="kota"
-                autoComplete="kota"
+                name="Kota"
+                id="Kota"
+                autoComplete="Kota"
                 class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                v-model="kota"
+                v-model="formData.Kota"
               />
             </div>
 
@@ -189,7 +217,7 @@ export default defineComponent({
                 id="postal-code"
                 autoComplete="postal-code"
                 class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                v-model="kodePos"
+                v-model="formData.KodePos"
               />
             </div>
 
@@ -206,7 +234,7 @@ export default defineComponent({
                 id="telepon"
                 autoComplete="telepon"
                 class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                v-model="telepon"
+                v-model="formData.Telepon"
               />
             </div>
 
@@ -223,7 +251,7 @@ export default defineComponent({
                 id="email-address"
                 autoComplete="email"
                 class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                v-model="email"
+                v-model="formData.Email"
               />
             </div>
 
@@ -246,7 +274,7 @@ export default defineComponent({
                   id="company-website"
                   class="block w-full flex-1 rounded-none rounded-r-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                   placeholder="www.example.com"
-                  v-model="website"
+                  v-model="formData.Website"
                 />
               </div>
             </div>
@@ -258,9 +286,10 @@ export default defineComponent({
               >
                 Tanggal Berdiri
               </label>
-              <VueDatePicker 
-                v-model="tanggalBerdiri"
+              <VueDatePicker
                 class="mt-1 block w-full rounded-md"
+                v-model="formData.TglMulai"
+                disabled
               ></VueDatePicker>
             </div>
 
@@ -277,7 +306,7 @@ export default defineComponent({
                 id="no-akta"
                 autoComplete="no-akta"
                 class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                v-model="noAkta"
+                v-model="formData.NoAkta"
               />
             </div>
 
@@ -294,7 +323,7 @@ export default defineComponent({
                 id="no-sah"
                 autoComplete="no-sah"
                 class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                v-model="noSah"
+                v-model="formData.NoSah"
               />
             </div>
 
@@ -304,7 +333,7 @@ export default defineComponent({
           <button
             type="submit"
             class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-            @click="updateIdentitas"
+            
           >
             Save
           </button>
