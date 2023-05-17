188ed26242dcf2eaf5f82810cd3be970822a5959
Monochrome Taisa <44545985+idm1303@users.noreply.github.com>
2023-05-17 22:47:34 +0800

    some update

--- src/views/Master/Program.vue
+++ src/views/Master/Program.vue
@@ -33,9 +33,9 @@ export default {
     <div class="bg-white rounded shadow overflow-hidden">
       <div class="flex justify-between items-center p-4">
         <h1>Programs</h1>
-        <button class="bg-indigo-600 rounded text-white px-4 py-2 text-sm">
-          Sample
-        </button>
+        <router-link :to="{name: 'Program Add'}" class="bg-indigo-600 rounded text-white px-4 py-2 text-sm">
+          Tambah Data
+        </router-link>
       </div>
       <div class="overflow-x-auto">
         <table class="w-full">
