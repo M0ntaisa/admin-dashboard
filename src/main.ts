7c507d26fae809e6d386231b7c2b9e238066b4c5
Monochrome Taisa <44545985+idm1303@users.noreply.github.com>
2023-05-18 22:20:45 +0800

    some update

---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import VueApexCharts from "vue3-apexcharts";
+import ApexCharts from "apexcharts";
+
+import App from "./App.vue";
+import router from "./router";
+
+import "./assets/main.css";
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+app.use(VueApexCharts);
+app.config.globalProperties.$apexcharts = ApexCharts;
+
+declare module "@vue/runtime-core" {
+  interface ComponentCustomProperties {
+    $apexcharts: typeof ApexCharts;
+  }
+}
+
+app.mount("#app");
+
+Vue.config.errorHandler = (err, vm, info) => {
+  console.error('Unhandled Vue error:', err);
+  console.error('Vue component:', vm);
+  console.error('Error info:', info);
+};
+
