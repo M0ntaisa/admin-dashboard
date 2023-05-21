07fceb99a440acfb31149f0ca233d39b906293d7
Monochrome Taisa <44545985+idm1303@users.noreply.github.com>
2023-05-21 09:16:46 +0800

    some update

---
+++ src/views/Master/ProgramStudiAdd.vue
@@ -0,0 +1,163 @@
+<script lang="ts">
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: 'MyComponent',
+    data() {
+      return {
+
+      };
+    },
+  });
+</script>
+
+<template>
+  <main class="bg-gray-100 p-4">
+    <form action="#" method="POST" class="mt-4">
+      <div class="shadow rounded overflow-hidden">
+        <div class="bg-white px-4 py-5 sm:p-6">
+          <div class="grid grid-cols-6 gap-6">
+            <div class="col-span-6 sm:col-span-3">
+              <label
+                htmlFor="first-name"
+                class="block text-sm font-medium text-gray-700"
+              >
+                First name
+              </label>
+              <input
+                type="text"
+                name="first-name"
+                id="first-name"
+                autoComplete="given-name"
+                class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              />
+            </div>
+
+            <div class="col-span-6 sm:col-span-3">
+              <label
+                htmlFor="last-name"
+                class="block text-sm font-medium text-gray-700"
+              >
+                Last name
+              </label>
+              <input
+                type="text"
+                name="last-name"
+                id="last-name"
+                autoComplete="family-name"
+                class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              />
+            </div>
+
+            <div class="col-span-6 sm:col-span-4">
+              <label
+                htmlFor="email-address"
+                class="block text-sm font-medium text-gray-700"
+              >
+                Email address
+              </label>
+              <input
+                type="text"
+                name="email-address"
+                id="email-address"
+                autoComplete="email"
+                class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              />
+            </div>
+
+            <div class="col-span-6 sm:col-span-3">
+              <label
+                htmlFor="country"
+                class="block text-sm font-medium text-gray-700"
+              >
+                Country
+              </label>
+              <select
+                id="country"
+                name="country"
+                autoComplete="country-name"
+                class="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+              >
+                <option>United States</option>
+                <option>Canada</option>
+                <option>Mexico</option>
+              </select>
+            </div>
+
+            <div class="col-span-6">
+              <label
+                htmlFor="street-address"
+                class="block text-sm font-medium text-gray-700"
+              >
+                Street address
+              </label>
+              <input
+                type="text"
+                name="street-address"
+                id="street-address"
+                autoComplete="street-address"
+                class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              />
+            </div>
+
+            <div class="col-span-6 sm:col-span-6 lg:col-span-2">
+              <label
+                htmlFor="city"
+                class="block text-sm font-medium text-gray-700"
+              >
+                City
+              </label>
+              <input
+                type="text"
+                name="city"
+                id="city"
+                autoComplete="address-level2"
+                class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              />
+            </div>
+
+            <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+              <label
+                htmlFor="region"
+                class="block text-sm font-medium text-gray-700"
+              >
+                State / Province
+              </label>
+              <input
+                type="text"
+                name="region"
+                id="region"
+                autoComplete="address-level1"
+                class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              />
+            </div>
+
+            <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+              <label
+                htmlFor="postal-code"
+                class="block text-sm font-medium text-gray-700"
+              >
+                ZIP / Postal code
+              </label>
+              <input
+                type="text"
+                name="postal-code"
+                id="postal-code"
+                autoComplete="postal-code"
+                class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              />
+            </div>
+          </div>
+        </div>
+        <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+          <button
+            type="submit"
+            class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          >
+            Save
+          </button>
+        </div>
+      </div>
+    </form>
+  </main>
+</template>
