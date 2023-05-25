6cf02612fd899f20d3cab1ce106f3c4623281ce2
Monochrome Taisa <44545985+idm1303@users.noreply.github.com>
2023-05-25 20:39:17 +0800

    some update

--- src/views/Master/ProgramStudiAdd.vue
+++ src/views/Master/ProgramStudiAdd.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, onMounted, computed } from 'vue';
+import { ref, onMounted } from 'vue';
 import useVuelidate from '@vuelidate/core';
 import { required, numeric } from '@vuelidate/validators';
 import router from '@/router';
